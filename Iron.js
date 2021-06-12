e55c5ad8b452b6b9b81da2d78f2fce98d32d6128
Mizumatsu111 <81099927+Mizumatsu111@users.noreply.github.com>
2021-06-12 10:42:40 -0500

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,25 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':30,
+        'friction': 3.0,
+        'restitution':0.8,
+      };
+      this.body = Bodies.rectangle(x, y, 60, 40, options);
+      this.width = 60;
+      this.height = 40;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      fill('orange')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
